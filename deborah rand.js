92cb8eac4af5bf1a08cd95721dbeef95ffa7743a
Deborah <deborah_huang@hotmail.com>
2017-08-26 12:11:24 -0400

    Update deborah rand.js

--- deborah rand.js
+++ deborah rand.js
@@ -6,8 +6,7 @@ var choice;
 var aList;
 var numPlace;
 var removeFromList;
-var stud1;
-var stud2;
+
 
 var students = [
   "Kevin",
@@ -45,8 +44,8 @@ document.addEventListener('DOMContentLoaded', () => {
   pickButton = document.getElementById('pickButton');
   pairButton = document.getElementById('pairButton');
   choice = document.getElementById('choice');
-  stud2 = document.getElementById('stud1');
-  stud2 = document.getElementById('stud2');
+  Copy1 = document.getElementById('Copy1');
+  Copy2 = document.getElementById('Copy2');
   aList = document.getElementById('aList');
 
   pickButton.addEventListener('click', pickOnClick );
@@ -78,19 +77,20 @@ var pickOnClick = function (event) {
 }
 
 var pairOnClick = function (event) {
-  stud1.innerHTML = '&nbsp;'
-  stud2.innerHTML = '&nbsp;'
-  var Copy1 = [];
-  var Copy2 = [];
-  Copy1 = students.slice(90, students.length/2);
-  Copy2 = students.slice(students.length/2+1,students.length); 
+var Copy1 = [];
+var Copy2 = [];
+  Copy1.innerHTML = '&nbsp;'
+  Copy2.innerHTML = '&nbsp;'
+  Copy1 = students.slice(0, students.length/2);
+  Copy2 = students.slice(((students.length/2)+1),students.length); 
   var rand1 = Copy1.splice(Math.floor(Math.random() * Copy1.length), 1);
-  var rand2 = Copy2.splice(Math.floor(Math.random() * Copy1.length), 1);
+  var rand2 = Copy2.splice(Math.floor(Math.random() * Copy2.length), 1);
   var x = window.setInterval(() => {
     if (colors[cur] === undefined) {
       window.clearInterval(x);
       cur = 0;
-      choice.innerText = rand;
+      Copy1.innerText = rand1;
+      Copy2.innerText = rand2;
       return;
     }
     if (colors[cur]) header.style.color = colors[cur];
