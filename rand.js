1fc425f08b3ccc6c78f706ddd5f6a70f0677f186
Deborah <deborah_huang@hotmail.com>
2017-08-26 14:49:14 -0400

    Update rand.js

--- rand.js
+++ rand.js
@@ -44,8 +44,6 @@ document.addEventListener('DOMContentLoaded', () => {
   pickButton = document.getElementById('pickButton');
   pairButton = document.getElementById('pairButton');
   choice = document.getElementById('choice');
-  Copy1 = document.getElementById('Copy1');
-  Copy2 = document.getElementById('Copy2');
   aList = document.getElementById('aList');
 
   pickButton.addEventListener('click', pickOnClick );
@@ -77,27 +75,21 @@ var pickOnClick = function (event) {
 }
 
 var pairOnClick = function (event) {
-
-   choice.innerHTML = '&nbsp;'
-var Copy2 = [];
-  Copy1.innerHTML = '&nbsp;'
-  Copy2.innerHTML = '&nbsp;'
-  Copy1 = students.slice(0, students.length/2);
-  Copy2 = students.slice(((students.length/2)+1),students.length); 
-  var rand1 = Copy1.splice(Math.floor(Math.random() * Copy1.length), 1);
-  var rand2 = Copy2.splice(Math.floor(Math.random() * Copy2.length), 1);
-  var x = window.setInterval(() => {
-    if (colors[cur] === undefined) {
-      window.clearInterval(x);
-      cur = 0;
-      Copy1.innerText = rand1;
-      Copy2.innerText = rand2;
-      return;
-    }
-    if (colors[cur]) header.style.color = colors[cur];
-    if (colors[cur-1]) body.style.backgroundColor = colors[cur-1];
-    cur += 1;
-  }, 200);
+choice.innerHTML = '&nbsp;'
+choice.innerHTML = '&nbsp;'
+var rand = students.splice(Math.floor(Math.random() * students.length),1);
+var x = window.setInterval(() => {
+  if (colors[cur] === undefined) {
+    window.clearInterval(x);
+    cur = 0;
+    choice.innerText = rand;
+    choice.innerTest = rand;
+    return;
+  }
+  if (colors[cur]) header.style.color = colors[cur];
+  if (colors[cur-1]) body.style.backgroundColor = colors[cur-1];
+  cur += 1;
+}, 200);
 }
 
 var aListOnClick = function (event) {
